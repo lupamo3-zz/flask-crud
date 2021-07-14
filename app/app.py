be163b01c3baed1d11a643c654ded9755b50d0ad
Lupamo Anjichi <thelupamo@gmail.com>
2021-07-14 09:47:41 +0300

    flask setup

---
+++ app/app.py
@@ -0,0 +1,21 @@
+
+from flask import Flask
+
+from .config import app_config
+
+
+def create_app(env_name):
+  
+  app = Flask(__name__)
+#   app initialization
+
+  app.config.from_object(app_config[env_name])
+
+  @app.route('/', methods=['GET'])
+  def index():
+    """
+    test  endpoint
+    """
+    return 'Congratulations!'
+
+  return app
